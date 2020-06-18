e04e8c9f8b62a9837775b04357c0553d94b3920b
LTgz <lux.theo@wanadoo.fr>
2020-06-18 13:31:05 +0200

    backbone agentcontroller

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+
+    private List<EnemyMovement> enemies = new List<EnemyMovement>();
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get reference to player location:
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        // Get enemies movement control scripts:
+        GameObject[] controlled = GameObject.FindGameObjectsWithTag("watermelon");
+        foreach (GameObject enemy in controlled) {
+            enemies.Add(enemy.GetComponent<EnemyMovement>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // For now just tell enemies to go to the player in a straight line:
+        foreach (EnemyMovement enemy in enemies) {
+            enemy.setWaypoint(player.position);
+        }
+    }
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,34 +5,43 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
 
-    private Transform player;
+    /*private Transform player;*/
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Vector3 waypoint;
     public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(Random.Range(0, 12), Random.Range(0, 12),0 );
         rb = this.GetComponent<Rigidbody2D>();
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        /*player = GameObject.FindGameObjectWithTag ("Player").transform;*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = player.position - transform.position;
+        /*Vector3 direction = player.position - transform.position;*/
+        Vector3 direction = waypoint - transform.position;
         //Debug.Log(direction);
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
         direction.Normalize();
         movement = direction;
     }
+
     private void FixedUpdate()
     {
         moveCharacter(movement);
     }
+
     void moveCharacter(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
+
+    public void setWaypoint(Vector3 waypoint) {
+        this.waypoint = waypoint;
+     }
 }
