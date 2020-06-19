407014fb3b057cce86a16a2cedb4f83fa04f50a6
Link <timelyeee@gmail.com>
2020-06-19 04:39:02 +0200

    Game Manager + Wander Script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+    private void MakeSingleton()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
